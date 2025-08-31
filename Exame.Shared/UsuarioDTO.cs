23398e43708c9567b1c5423196939380b325cd97
Breno <brenomansur@gmail.com>
2025-08-31 20:45:11 -0300

    Inclusão de DTO

---
+++ Exame.Shared/UsuarioDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace Exame.Shared
+{
+    public class UsuarioDTO
+    {
+        public Guid Id { get; set; }
+        public string Nome { get; set; } = default!;
+        public string Endereco { get; set; } = default!;
+        public DateTime DataNascimento { get; set; }
+        public int Idade { get; set; }
+    }
+}
